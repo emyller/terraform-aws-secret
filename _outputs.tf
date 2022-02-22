58020ebf14bcd24065f946717d3bd0643bb571d1
Evandro Myller <emyller@d7.dev>
2022-02-22 16:53:48 -0300

    Output the secret name

--- _outputs.tf
+++ _outputs.tf
@@ -2,6 +2,10 @@ output "arn" {
   value = aws_secretsmanager_secret.main.arn
 }
 
+output "name" {
+  value = aws_secretsmanager_secret.main.name
+}
+
 output "contents" {
   value = local.contents
   sensitive = true
